151667b20b9ae772a49cde2f6e019e3b374369b7
ofthesun9 <olivier@ofthesun.net>
2020-06-17 10:26:54 +0200

    Update deploy.sh

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 docker login -u $DOCKER_UN -p $DOCKER_PW
+docker push ofthesun9/unbound-mailu:amd64-${PKG_VERSION}
+docker push ofthesun9/unbound-mailu:arm64v8-${PKG_VERSION}
 DOCKER_CLI_EXPERIMENTAL=enabled docker manifest create ofthesun9/unbound-mailu:${PKG_VERSION}  \
           ofthesun9/unbound-mailu:amd64-${PKG_VERSION}  \
           ofthesun9/unbound-mailu:arm64v8-${PKG_VERSION}
